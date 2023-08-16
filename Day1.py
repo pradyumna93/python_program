c9c80e67d3c597218459a50064d0483a6a6855f4
Pradyumna <pradyumnadas1993@gmail.com>
2023-08-16 08:22:40 +0530

    Day1 of python learning

---
+++ Day1.py
@@ -0,0 +1,64 @@
+print ("Hello, World!")
+# It will print the 2 Hello world in new line
+
+
+# Exercise - 1
+# The output should be like below 
+
+# Day 1 - Python Print Function
+# The function is declared like this:       
+# print('what to print')
+
+print ("Day 1 - Python Print Function")
+print ("The function is declared like this:")
+print ("print('what to print')")
+
+#Exercise -2
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+# correct the IndentationError
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+print ("Hello World\nHello world")
+
+# It will print the space between Hello and angle
+print ("Hello" + "  " + "Angel")
+# Input() will get input in console 
+# Then print () will print the word "Hello" and the user input
+print ("Hello " + input("What is your Name? ") + "!")
+
+#Exercise - 3
+# Print the length of the input
+
+user_name = input("")
+print(len(user_name))
+
+#Exercise-4
+# Switch "a" value to "b"
+
+a = input ("a: ")
+b = input ("b: ")
+c = a
+a = b
+b = c
+print ("a: ", a)
+print ("b: ", b)
+
+#Exercise -5
+# Create the band name generater
+
+print ("Welcome to the Band name generator.")
+city = input("Enter your city name : ")
+pet = input("Enter your pet name : ")
+print ("Your band name is ", city + pet)
+
+
